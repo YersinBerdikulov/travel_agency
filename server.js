4647eecb0e3900fb54ac8181545ac33b21a3e5a9
Yersin Berdikulov <221225@astanait.edu.kz>
2024-01-29 03:42:46 +0600

    Bug fixes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const axios = require('axios');
+
+
 const app = express();
 
 const port = 3000;
 
 const travelRoutes = require('./routes/travelRoutes');
-
 
 app.use(express.static(__dirname + "/public"));
 
